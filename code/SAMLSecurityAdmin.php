857ddee0db1d86e797b2d57d745bea9b5f8bfbae
spekulatius <petert@catalyst.net.nz>
2015-12-13 16:50:14 +1300

    Converted to PSR-2

--- code/SAMLSecurityAdmin.php
+++ code/SAMLSecurityAdmin.php
@@ -5,130 +5,136 @@
  * @package framework
  * @subpackage admin
  */
-class SAMLSecurityAdmin extends LeftAndMain implements PermissionProvider {
-	/**
-	 * @var string
-	 */
-	private static $url_segment = 'saml-security';
+class SAMLSecurityAdmin extends LeftAndMain implements PermissionProvider
+{
+    /**
+     * @var string
+     */
+    private static $url_segment = 'saml-security';
 
-	/**
-	 * @var string
-	 */
-	private static $url_rule = '/$Action/$ID/$OtherID';
+    /**
+     * @var string
+     */
+    private static $url_rule = '/$Action/$ID/$OtherID';
 
-	/**
-	 * @var string
-	 */
-	private static $menu_title = 'SAML';
+    /**
+     * @var string
+     */
+    private static $menu_title = 'SAML';
 
-	/**
-	 * @var string
-	 */
-	private static $menu_icon = '/silverstripe-saml-sp/images/key.png';
+    /**
+     * @var string
+     */
+    private static $menu_icon = '/silverstripe-saml-sp/images/key.png';
 
-	/**
-	 * @var string
-	 */
-	private static $subitem_class = 'Saml';
+    /**
+     * @var string
+     */
+    private static $subitem_class = 'Saml';
 
-	/**
-	 * @var array
-	 */
-	private static $allowed_actions = array(
-		'EditForm',
-		'saml',
-	);
+    /**
+     * @var array
+     */
+    private static $allowed_actions = array(
+        'EditForm',
+        'saml',
+    );
 
-	public function init() {
-		parent::init();
-		Requirements::javascript(FRAMEWORK_ADMIN_DIR . '/javascript/SecurityAdmin.js');
-	}
+    public function init()
+    {
+        parent::init();
+        Requirements::javascript(FRAMEWORK_ADMIN_DIR . '/javascript/SecurityAdmin.js');
+    }
 
-	/**
-	 * Shortcut action for setting the correct active tab.
-	 */
-	public function saml($request) {
-		return $this->index($request);
-	}
+    /**
+     * Shortcut action for setting the correct active tab.
+     */
+    public function saml($request)
+    {
+        return $this->index($request);
+    }
 
-	public function getEditForm($id = null, $fields = null) {
-		// @TODO Duplicate record fetching (see parent implementation)
-		if(!$id) $id = $this->currentPageID();
-		$form = parent::getEditForm($id);
+    public function getEditForm($id = null, $fields = null)
+    {
+        // @TODO Duplicate record fetching (see parent implementation)
+        if (!$id) {
+            $id = $this->currentPageID();
+        }
+        $form = parent::getEditForm($id);
 
-		// @TODO Duplicate record fetching (see parent implementation)
-		$record = $this->getRecord($id);
+        // @TODO Duplicate record fetching (see parent implementation)
+        $record = $this->getRecord($id);
 
-		if($record && !$record->canView()) {
-			return Security::permissionFailure($this);
-		}
+        if ($record && !$record->canView()) {
+            return Security::permissionFailure($this);
+        }
 
-		// Add import capabilities. Limit to admin since the import logic can affect assigned permissions
-		if (Permission::check('ADMIN')) {
-			$samlField = GridField::create('SAML',
-				false,
-				IdentityProvider::get(),
-				GridFieldConfig_RecordEditor::create()
-			);
+        // Add import capabilities. Limit to admin since the import logic can affect assigned permissions
+        if (Permission::check('ADMIN')) {
+            $samlField = GridField::create('SAML',
+                false,
+                IdentityProvider::get(),
+                GridFieldConfig_RecordEditor::create()
+            );
 
-			$dataColumns = $samlField->getConfig()->getComponentByType('GridFieldDataColumns');
-			$dataColumns->setDisplayFields(array(
-				'Title' => 'Title',
-				'Active' => 'Active',
-				'entityid'=> 'Entity ID',
-			));
+            $dataColumns = $samlField->getConfig()->getComponentByType('GridFieldDataColumns');
+            $dataColumns->setDisplayFields(array(
+                'Title' => 'Title',
+                'Active' => 'Active',
+                'entityid'=> 'Entity ID',
+            ));
 
-			$fields = new FieldList(
-				$root = new TabSet(
-					'Root',
-					$samlTab = new Tab('SAML', _t('SAMLSecurityAdmin.SAML', 'SAML'),
-						$samlField,
-						new LiteralField('SAMLCautionText',
-							sprintf('<p class="caution-remove"><strong>%s</strong></p>',
-								_t(
-									'SAMLSecurityAdmin.SAMLCautionText',
-									'Caution: Pages that require authentication will be re-directed to Identity Provder'
-								)
-							)
-						)
-					)
-				),
+            $fields = new FieldList(
+                $root = new TabSet(
+                    'Root',
+                    $samlTab = new Tab('SAML', _t('SAMLSecurityAdmin.SAML', 'SAML'),
+                        $samlField,
+                        new LiteralField('SAMLCautionText',
+                            sprintf('<p class="caution-remove"><strong>%s</strong></p>',
+                                _t(
+                                    'SAMLSecurityAdmin.SAMLCautionText',
+                                    'Caution: Pages that require authentication will be re-directed to Identity Provder'
+                                )
+                            )
+                        )
+                    )
+                ),
 
-				// necessary for tree node selection in LeftAndMain.EditForm.js
-				new HiddenField('ID', false, 0)
-			);
-		}
+                // necessary for tree node selection in LeftAndMain.EditForm.js
+                new HiddenField('ID', false, 0)
+            );
+        }
 
-		// Tab nav in CMS is rendered through separate template
-		$root->setTemplate('CMSTabSet');
+        // Tab nav in CMS is rendered through separate template
+        $root->setTemplate('CMSTabSet');
 
 
-		$actionParam = $this->request->param('Action');
-		if($actionParam == 'saml') {
-			$groupsTab->addExtraClass('ui-state-active');
-		}
+        $actionParam = $this->request->param('Action');
+        if ($actionParam == 'saml') {
+            $groupsTab->addExtraClass('ui-state-active');
+        }
 
-		$actions = new FieldList();
+        $actions = new FieldList();
 
-		$form = CMSForm::create(
-			$this,
-			'EditForm',
-			$fields,
-			$actions
-		)->setHTMLID('Form_EditForm');
-		$form->setResponseNegotiator($this->getResponseNegotiator());
-		$form->addExtraClass('cms-edit-form');
-		$form->setTemplate($this->getTemplatesWithSuffix('_EditForm'));
+        $form = CMSForm::create(
+            $this,
+            'EditForm',
+            $fields,
+            $actions
+        )->setHTMLID('Form_EditForm');
+        $form->setResponseNegotiator($this->getResponseNegotiator());
+        $form->addExtraClass('cms-edit-form');
+        $form->setTemplate($this->getTemplatesWithSuffix('_EditForm'));
 
-		// Tab nav in CMS is rendered through separate template
-		if($form->Fields()->hasTabset()) {
-			$form->Fields()->findOrMakeTab('Root')->setTemplate('CMSTabSet');
-		}
-		$form->addExtraClass('center ss-tabset cms-tabset ' . $this->BaseCSSClasses());
-		$form->setAttribute('data-pjax-fragment', 'CurrentForm');
+        // Tab nav in CMS is rendered through separate template
+        if ($form->Fields()->hasTabset()) {
+            $form->Fields()->findOrMakeTab('Root')->setTemplate('CMSTabSet');
+        }
+        $form->addExtraClass('center ss-tabset cms-tabset ' . $this->BaseCSSClasses());
+        $form->setAttribute('data-pjax-fragment', 'CurrentForm');
 
-		$this->extend('updateEditForm', $form);
+        $this->extend('updateEditForm', $form);
 
-		return $form;
-	}
+        return $form;
+    }
 }
